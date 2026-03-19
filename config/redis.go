adfc11eb818b36b8d5abe3095ed507863b36bf51
Ayam Kuda <kadekyudaputra17@gmail.com>
2026-03-19 15:03:05 +0800

    add tls redis

--- config/redis.go
+++ config/redis.go
@@ -3,6 +3,7 @@ package config
 import (
 	"chronosphere/utils"
 	"context"
+	"crypto/tls"
 	"log"
 
 	"github.com/redis/go-redis/v9"
@@ -13,6 +14,9 @@ func InitRedisDB(addr, password string, db int) *redis.Client {
 		Addr:     addr,
 		Password: password,
 		DB:       db,
+		TLSConfig: &tls.Config{
+			MinVersion: tls.VersionTLS12,
+		},
 	})
 
 	if _, err := rdb.Ping(context.Background()).Result(); err != nil {
