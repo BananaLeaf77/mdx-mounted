7799373443f7d322aa8e9502badc34c80152eddb
BananaLeaf77 <dognub61@gmail.com>
2026-03-30 14:11:13 +0800

    update

--- config/redis.go
+++ config/redis.go
@@ -5,18 +5,24 @@ import (
 	"context"
 	"crypto/tls"
 	"log"
+	"os"
 
 	"github.com/redis/go-redis/v9"
 )
 
 func InitRedisDB(addr, password string, db int) *redis.Client {
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     addr,
-		Password: password,
-		DB:       db,
-		TLSConfig: &tls.Config{
+	var tlsConfig *tls.Config
+	if os.Getenv("APP_ENV") == "production" || os.Getenv("REDIS_TLS") == "true" {
+		tlsConfig = &tls.Config{
 			MinVersion: tls.VersionTLS12,
-		},
+		}
+	}
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:      addr,
+		Password:  password,
+		DB:        db,
+		TLSConfig: tlsConfig,
 	})
 
 	if _, err := rdb.Ping(context.Background()).Result(); err != nil {
