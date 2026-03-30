b5d2205ab18c15ee9e8ea23de834b376fa888444
BananaLeaf77 <dognub61@gmail.com>
2026-03-30 13:13:34 +0800

    redis

--- config/redis.go
+++ config/redis.go
@@ -3,7 +3,7 @@ package config
 import (
 	"chronosphere/utils"
 	"context"
-	// "crypto/tls"
+	"crypto/tls"
 	"log"
 
 	"github.com/redis/go-redis/v9"
@@ -14,9 +14,9 @@ func InitRedisDB(addr, password string, db int) *redis.Client {
 		Addr:     addr,
 		Password: password,
 		DB:       db,
-		// TLSConfig: &tls.Config{
-		// 	MinVersion: tls.VersionTLS12,
-		// },
+		TLSConfig: &tls.Config{
+			MinVersion: tls.VersionTLS12,
+		},
 	})
 
 	if _, err := rdb.Ping(context.Background()).Result(); err != nil {
