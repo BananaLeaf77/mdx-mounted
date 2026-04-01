1859690808ffbfc3a347d736f5b8d51803671248
Ayam Kuda <kadekyudaputra17@gmail.com>
2026-04-01 19:08:19 +0800

    test dto, repo album update teacher

--- delivery/upload.go
+++ delivery/upload.go
@@ -4,7 +4,6 @@ import (
 	"chronosphere/config"
 	"chronosphere/utils"
 	"crypto/sha1"
-	"encoding/hex"
 	"fmt"
 	"net/http"
 	"os"
@@ -38,12 +37,15 @@ func (h *UploadHandler) GenerateSignature(c *gin.Context) {
 	}
 
 	// Format string yang diminta Cloudinary sebelum di-hash
-	dataToSign := fmt.Sprintf("timestamp=%d%s", timestamp, apiSecret)
+	// We include folder=album_teacher because frontend needs it for Teacher Album
+	// Note: All params must be sorted alphabetically by Cloudinary rules,
+	// but since we only have 'folder' and 'timestamp', 'folder' (f) comes before 'timestamp' (t).
+	dataToSign := fmt.Sprintf("folder=album_teacher&timestamp=%d%s", timestamp, apiSecret)
 
 	// Hasilkan hash SHA-1
 	hash := sha1.New()
 	hash.Write([]byte(dataToSign))
-	signature := hex.EncodeToString(hash.Sum(nil))
+	signature := fmt.Sprintf("%x", hash.Sum(nil))
 
 	// Kembalikan ke frontend
 	c.JSON(http.StatusOK, gin.H{
